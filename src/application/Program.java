49fd8e8140533ce2861e42e88c37817fc9896e59
Kristian <kristianlll@hotmail.com>
2025-08-27 20:00:50 -0300

    Implementação das classes utilitárias para conexão com o mySQL

---
+++ src/application/Program.java
@@ -0,0 +1,43 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import db.DB;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.createStatement();
+			
+			rs= st.executeQuery("SELECT * FROM department");
+			
+			while (rs.next()) {
+				System.out.println(rs.getInt("Id") + " " + rs.getString("Name"));
+			}
+			
+		} catch (SQLException err) {
+			System.out.println(err.getMessage());
+		} finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+		
+		
+//		Connection conn = DB.getConnection();
+//		DB.closeConnection();
+//		System.out.println("Deu certo");
+	}
+
+}
