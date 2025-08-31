1ab4e8918437e4506c2bb412d80d4116f77e7614
KristianPC <kristianlll@hotmail.com>
2025-08-30 23:10:39 -0300

    Implementação classe entidade Departamento

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,14 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.SQLIntegrityConstraintViolationException;
-import java.sql.Statement;
-
-import db.DB;
-import db.DbException;
-import db.DbIntegrityException;
+import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "Marketing");
+		
+		System.out.println(obj);
 		
 //		Connection conn = null;
 
@@ -138,42 +134,42 @@ public class Program {
 //		}
 		
 		// transação
-		Connection conn = null;
-		Statement st = null;
-		
-		try {
-			conn = DB.getConnection();
-			
-			conn.setAutoCommit(false);
-			
-			st = conn.createStatement();
-		
-			
-			int rows1 = st.executeUpdate("UPDATE seller set BaseSalary = 2090 WHERE DepartmentId = 1");
-
-			int x = 1;
-			if (x<2) {
-				throw new SQLException("Fake error");
-			}
-			
-			int rows2 = st.executeUpdate("UPDATE seller set BaseSalary = 3090 DepartmentId = 2");
-			
-			conn.commit();
-			
-			System.out.println("rows1 " + rows1);
-			System.out.println("rows2 " + rows2);
-			
-		} catch (SQLException e) {
-			try {
-				conn.rollback();
-				throw new DbException("Transaction rolled back! Caused by " + e.getMessage());
-			} catch (SQLException e1) {
-				throw new DbException("Error trying to rollbac! Caused by " + e1.getMessage());
-			}
-		} finally {
-			DB.closeStatement(st);
-			DB.closeConnection();
-		}
+//		Connection conn = null;
+//		Statement st = null;
+//		
+//		try {
+//			conn = DB.getConnection();
+//			
+//			conn.setAutoCommit(false);
+//			
+//			st = conn.createStatement();
+//		
+//			
+//			int rows1 = st.executeUpdate("UPDATE seller set BaseSalary = 2090 WHERE DepartmentId = 1");
+//
+//			int x = 1;
+//			if (x<2) {
+//				throw new SQLException("Fake error");
+//			}
+//			
+//			int rows2 = st.executeUpdate("UPDATE seller set BaseSalary = 3090 DepartmentId = 2");
+//			
+//			conn.commit();
+//			
+//			System.out.println("rows1 " + rows1);
+//			System.out.println("rows2 " + rows2);
+//			
+//		} catch (SQLException e) {
+//			try {
+//				conn.rollback();
+//				throw new DbException("Transaction rolled back! Caused by " + e.getMessage());
+//			} catch (SQLException e1) {
+//				throw new DbException("Error trying to rollbac! Caused by " + e1.getMessage());
+//			}
+//		} finally {
+//			DB.closeStatement(st);
+//			DB.closeConnection();
+//		}
 		
 		
 		
