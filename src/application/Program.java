6e1ffed14fa07bfc2040f0faf556a37485d666c6
KristianPC <kristianlll@hotmail.com>
2025-08-30 23:33:15 -0300

    Implementação das interfaces dao de vendedor e departamento

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,6 +12,10 @@ public class Program {
 		Department obj = new Department(1, "Marketing");
 		
 		System.out.println(obj);
+		
+		Seller s = new Seller(1, "a", "a", new Date(), 3200.0, obj);
+		
+		System.out.println(s);
 		
 //		Connection conn = null;
 
